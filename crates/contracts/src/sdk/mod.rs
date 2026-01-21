890da72912cc2b47fae42fa4f12998496a79445c
Illia Kripaka <kripakaillya@gmail.com>
2026-01-21 14:51:09 +0200

    Add PartialPset implementation for tx finalization both inplace and manual for custom behavior

--- crates/contracts/src/sdk/mod.rs
+++ crates/contracts/src/sdk/mod.rs
@@ -6,7 +6,13 @@ mod finance;
 pub mod taproot_pubkey_gen;
 pub mod validation;
 
+mod partial_pset;
+mod signer;
+
 #[cfg(feature = "sdk-basic")]
 pub use basic::*;
 #[cfg(any(feature = "finance-option-offer", feature = "finance-options"))]
 pub use finance::*;
+
+pub use partial_pset::*;
+pub use signer::*;
